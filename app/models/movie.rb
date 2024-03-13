d27340b05483dc2021fdcffcb7af88460ceb9009
Ruy R. <108208+ruyrocha@users.noreply.github.com>
2024-03-13 15:49:00 -0300

    Add all movie associations, and some of them are wrong.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class Movie < ApplicationRecord
+  has_many :actors
+  has_many :countries
+  has_many :crews
+  has_many :genres
+  has_many :languages
+  has_many :releases
+  has_many :studios
+  has_many :themes
 end
