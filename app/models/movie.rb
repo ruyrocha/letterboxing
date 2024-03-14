8da81030bb903137a00e523e168d0fe9d909c317
Ruy R. <108208+ruyrocha@users.noreply.github.com>
2024-03-14 15:32:41 -0300

    Remove `has_many` associations on Movie.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
-  has_many :actors
-  has_many :countries
-  has_many :crews
-  has_many :genres
-  has_many :languages
-  has_many :releases
-  has_many :studios
-  has_many :themes
 end
